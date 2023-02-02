2f72f5ff32e260037958942232f872eb3532e203
Digambar <deshawaldigambar@gmail.com>
2023-02-02 12:20:27 +0530

    variable declaration, types

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var name = "digambar welcome back to the team";
+// console.log(name);
+// variable declaration 
+var x = 10;
+var y = 5;
+var title = "Codevolution";
+// variable types
+var isBegginer = true;
+var total = 100;
+var place = "Belgaum";
+var sentence = "my tite is ".concat(title);
+console.log(sentence);
---
+++ main.ts
@@ -0,0 +1,17 @@
+export {}   //tst treats this file as module instaed of script
+let name ="digambar welcome back to the team";
+// console.log(name);
+
+// variable declaration 
+let x=10;
+let y=5;
+const title ="Codevolution"
+
+
+// variable types
+let isBegginer :boolean = true;
+let total:number = 100;
+let place:string ="Belgaum"
+let sentence :string = `my tite is ${title}`
+console.log(sentence)
+
